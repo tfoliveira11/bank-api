2d20689ef9ed9e0bedeb8386f3afc2d0ca48667e
tfoliveira <tfoliveira11@gmail.com>
2023-10-11 12:03:16 -0300

    init commit

---
+++ src/main/kotlin/com/challenge/bank/BankPaiApplication.kt
@@ -0,0 +1,11 @@
+package com.challenge.bank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankPaiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankPaiApplication>(*args)
+}
